dcff0812466874b981a21cb3d484b2503c553082
TitanProgrammer4480 <sebi-2007@web.de>
2025-01-30 18:14:08 +0100

    added Fav Tech

--- test/src/components/Header.jsx
+++ test/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div>
           <h1> { String.fromCodePoint(9866) } Hi, I&apos;m Sebastian { String.fromCodePoint(9866) } </h1>
           <h2>I&apos;m a <span className="text-accent-c">fullstack developer</span> <br /> and have been coding <span className="text-accent-c">since 2019</span>.</h2>
-          <a className="bg-accent-c text-text-c inline-block no-underline mt-2 pt-2 pr-5 pb-2 pl-5 border-2 rounded-3xl border-accent-c border-solid" href="https://github.com/TitanProgrammer4480" target="_blank">Github</a>
+          <a className="bg-accent-c text-text-c inline-block no-underline mt-2 pt-1 pr-5 pb-1 pl-5 border-2 rounded-3xl border-accent-c border-solid" href="https://github.com/TitanProgrammer4480" target="_blank">Github</a>
         </div>
     </header>
   )
--- test/src/components/TechStack.jsx
+++ test/src/components/TechStack.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
 
 function TechStack() {
+
+  const stuff = [
+    {
+      name: "Python",
+      link: "https://www.python.org",
+      desc: "A very powerful widely used Programming language"
+    },
+    {
+      name: "React",
+      link: "https://react.dev",
+      desc: "A common js-framework for building web applications"
+    },
+    {
+      name: "Firebase",
+      link: "https://firebase.google.com",
+      desc: "A tool for developers to add a cloud or database to their projects"
+    },
+    {
+      name: "MongoDB",
+      link: "https://www.mongodb.com",
+      desc: "A no-sql database which has a wide variety of wrapper for the most common languages"
+    },
+    {
+      name: "Flask",
+      link: "https://de.wikipedia.org/wiki/Flask",
+      desc: "A framework for building api/web applications in python"
+    },
+  ]
+
   return (
-    <div>TechStack</div>
+    <section className='h-screen w-full bg-text-c text-black flex flex-col items-center justify-center snap-start font-display'>
+      <div className="w-full h-[5%] bg-black rounded-bl-[100%] rounded-br-[100%]"></div>
+      <div className="w-full h-[95%] flex flex-col justify-center items-center">
+        <div>
+          <h2 className='overline'>Favourite Technologies:</h2>
+        </div>
+        <div>
+          <div className="mt-4 pt-2 pb-2 pl-10 bg-white rounded-[20px]">
+            <h3 className='underline'><a href="https://www.python.org" target="_blank">Python</a></h3>
+            <p>A very powerful widely used Programming language</p>
+          </div>
+          <div className="mt-4 pt-2 pb-2 pl-10 bg-white rounded-[20px]">
+            <h3 className='underline'><a href="https://react.dev" target="_blank">React</a></h3>
+            <p>A common js-framework for building web applications</p>
+          </div>
+          <div className="mt-4 pt-2 pb-2 pl-10 bg-white rounded-[20px]">
+            <h3 className='underline'><a href="https://firebase.google.com" target="_blank">Firebase</a></h3>
+            <p>A tool for developers to add a cloud or database to their projects</p>
+          </div>
+          <div className="mt-4 pt-2 pb-2 pl-10 bg-white rounded-[20px]">
+            <h3 className='underline'><a href="https://www.python.org" target="_blank">MongoDB</a></h3>
+            <p>A no-sql database which has a wide variety of wrapper for the most common languages</p>
+          </div>
+          <div className="mt-4 pt-2 pb-2 pl-10 bg-white rounded-[20px]">
+            <h3 className='underline'><a href="https://de.wikipedia.org/wiki/Flask" target="_blank">Flask</a></h3>
+            <p>A framework for building api/web applications in python</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
