7992e90284c7d8c5c28a57326ee646943b1b0ad3
TitanProgrammer4480 <sebi-2007@web.de>
2025-01-30 17:41:04 +0100

    fixed header

--- test/src/components/Header.jsx
+++ test/src/components/Header.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className='bg_color t_color h-screen w-full flex flex-col items-center justify-center snap-start'>
+    <header className='bg-black text-text-c h-screen w-full flex flex-col items-center justify-center snap-start font-display'>
         <div>
           <h1> { String.fromCodePoint(9866) } Hi, I&apos;m Sebastian { String.fromCodePoint(9866) } </h1>
-          <h2>I&apos;m a <span className="ac_color">fullstack developer</span> <br /> and have been coding <span className="ac_color">since 2019</span>.</h2>
-          <a className="h-20 bg-[#007FFF] text-[#f5f5f0] no-underline pt-5 pr-15 pb-5 pl-15 border-2 rounded-3xl border-[#007FFF] border-solid" href="https://github.com/TitanProgrammer4480" target="_blank">Github</a>
-        </div>
-        <div id="navbar">
-          <a class="btn" href="#tech-stack">Tech Stack</a>
-          <a class="btn" href="#projects">Project</a>
+          <h2>I&apos;m a <span className="text-accent-c">fullstack developer</span> <br /> and have been coding <span className="text-accent-c">since 2019</span>.</h2>
+          <a className="bg-accent-c text-text-c inline-block no-underline mt-2 pt-2 pr-5 pb-2 pl-5 border-2 rounded-3xl border-accent-c border-solid" href="https://github.com/TitanProgrammer4480" target="_blank">Github</a>
         </div>
     </header>
   )
