f16d45a5f577218824855b5bd9dd5b01212c8f02
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2025-02-07 11:29:17 +0100

    Update ProjectItem.jsx

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -4,7 +4,7 @@ import Button from './Button';
 function ProjectItem({id, name, desc, githubLink, exampleLink, exampleSite}) {
 
   return (
-    <li key={id} className='text-text-c mt-4 mr-4 ml-4 pt-2 pb-2 pl-8 pr-8 rounded-[20px] border-6 border-accent-c border-solid'>
+    <li key={id} className='text-text-c mt-4 mr-4 ml-4 pt-2 pb-2 pl-4 pr-4 rounded-[20px] border-6 border-accent-c border-solid'>
         <h3>{name}</h3>
         <div>
             <Button Text="Code" Link={githubLink} />
