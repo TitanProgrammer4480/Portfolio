6a06429acc65dfe9ae1850b2662cde1656d0ef39
TitanProgrammer4480 <sebi-2007@web.de>
2025-02-12 19:02:56 +0100

    added supabase fetching

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,26 @@
 import { React, useEffect, useState } from 'react'
+import { createClient } from "@supabase/supabase-js"
 import ProjectItem from './ProjectItem'
-import { projects } from "../assets/Constants"
+// import { projects } from "../assets/Constants"
+
+const supabase = createClient(import.meta.env.VITE_PROJECT_URL, import.meta.env.VITE_ANON_KEY)
 
 function Projects() {
 
-  const projectList = projects.map((id, name, desc, githubLink, exampleLink,exampleSite) => ProjectItem(id, name, desc, githubLink, exampleLink,exampleSite))
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  async function getProjects() {
+    const { data } = await supabase.from("projects").select();
+    setProjects(data);
+  }
+
+  const projectList = projects.map((id, name, desc, githubLink, exampleLink, exampleSite) => ProjectItem(id, name, desc, githubLink, exampleLink, exampleSite))
+
+  // const projectList = projects.map((id, name, desc, githubLink, exampleLink,exampleSite) => ProjectItem(id, name, desc, githubLink, exampleLink,exampleSite))
 
   return (
     <div className='w-full min-h-screen bg-black text-text-c flex flex-col items-center justify-center snap-start'>
