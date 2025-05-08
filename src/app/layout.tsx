7eb0f17e7d164492c0359beb22a97473031aa822
TitanProgrammer4480 <sebi-2007@web.de>
2025-05-08 15:08:02 +0200

    changed hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Arsenal } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const arsenal = Arsenal({
+  subsets: ['latin'],
+  weight: "700",
+  style: "normal"
 });
 
 export const metadata: Metadata = {
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${arsenal.className} antialiased`}
       >
         {children}
       </body>
