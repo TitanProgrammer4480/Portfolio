cd4d779e1d36f8b82a239151b41b327ff2208137
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2025-05-15 08:47:13 +0200

    Update ProjectSkeleton.tsx

--- src/components/skeletons/ProjectSkeleton.tsx
+++ src/components/skeletons/ProjectSkeleton.tsx
@@ -6,7 +6,7 @@ const ProjectSkeleton = () => {
       <div className='mb-10'>
         <h2 className='text-3xl overline'>Projects:</h2>
       </div>
-      <div className='grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3'>
+      <div className='grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3 animate-pulse'>
         <p>Loading...</p>
       </div>
     </div>
